a84917095e51a2b5dfb057679e4d8eff8ba27f86
johnathanCamelo <48776908+johnathanCamelo@users.noreply.github.com>
2019-04-04 11:54:41 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   URLCatalog: "http://ab13b1006538611e9ba830a1db711ca4-631194424.us-east-2.elb.amazonaws.com:3010",
-  URLLogin: "http://a509c9b1d537311e9a81b0666116d3a5-342058073.us-east-2.elb.amazonaws.com:18080",
+  URLLogin: "https://a509c9b1d537311e9a81b0666116d3a5-342058073.us-east-2.elb.amazonaws.com",
   URLSecurity: "http://aaa01c7ec542511e9a81b0666116d3a5-63920683.us-east-2.elb.amazonaws.com:3000",
   URLCard: "http://a4997aa3e537811e9a81b0666116d3a5-931031808.us-east-2.elb.amazonaws.com:18080",
   URLOrder: "http://acabea643537511e9a81b0666116d3a5-432135908.us-east-2.elb.amazonaws.com:18080",
