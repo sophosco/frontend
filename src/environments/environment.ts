fec6107d1c066f074f7850f599de6a92d0eae689
CristianDallos <cristiandaviddallosbustos@gmail.com>
2019-04-05 11:59:03 -0500

    Ajustes en consumos por https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  URLCatalog: "http://ab13b1006538611e9ba830a1db711ca4-631194424.us-east-2.elb.amazonaws.com:3010",
+  URLCatalog: "https://ab13b1006538611e9ba830a1db711ca4-631194424.us-east-2.elb.amazonaws.com",
   URLLogin: "https://a509c9b1d537311e9a81b0666116d3a5-342058073.us-east-2.elb.amazonaws.com",
-  URLSecurity: "https://aaa01c7ec542511e9a81b0666116d3a5-63920683.us-east-2.elb.amazonaws.com",
-  URLCard: "http://a4997aa3e537811e9a81b0666116d3a5-931031808.us-east-2.elb.amazonaws.com:18080",
-  URLOrder: "http://acabea643537511e9a81b0666116d3a5-432135908.us-east-2.elb.amazonaws.com:18080",
-  URLPayment: "http://adb958412537911e9ba830a1db711ca4-1645037395.us-east-2.elb.amazonaws.com:18080",
+  URLSecurity: "httpss://aaa01c7ec542511e9a81b0666116d3a5-63920683.us-east-2.elb.amazonaws.com",
+  URLCard: "https://a4997aa3e537811e9a81b0666116d3a5-931031808.us-east-2.elb.amazonaws.com",
+  URLOrder: "https://acabea643537511e9a81b0666116d3a5-432135908.us-east-2.elb.amazonaws.com",
+  URLPayment: "https://adb958412537911e9ba830a1db711ca4-1645037395.us-east-2.elb.amazonaws.com",
   endPointLogin: "/api/users/login/",
   endPointCatalog: "/api/products/catalog/",
   endPointDetailProduc: "/api/products/product/",
